573816f52fd493cded9eecd6e322c33fd62c6b7b
Kieran Dominy <38659105+MrKren@users.noreply.github.com>
2021-12-08 08:21:38 +0000

    Add selected to nav bar (#3)

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,12 @@
+import { Divider } from "@mui/material";
 import { Box } from "@mui/system";
 import ExampleText from "../ExampleText";
 
 function About() {
   return (
-    <Box sx={{
-      textAlign: "center"
-    }}>
-    <h2>About Page</h2>
+    <Box>
+    <h1>About Page</h1>
+    <Divider />
     <p>This section is about ...</p>
     <ExampleText paras={20}></ExampleText>
     </Box>
