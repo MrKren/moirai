71afbaa02e7ffdb4d32922ca9a9a47667024975c
Kieran Dominy <38659105+MrKren@users.noreply.github.com>
2022-01-27 23:35:49 +0000

    Editor templates (#9)
    * add template icons
    
    * Set markdown flavor to github
    
    * add contents box
    
    * fix use effect in example text

--- src/ContentEditor/ContentEditor.js
+++ src/ContentEditor/ContentEditor.js
@@ -1,11 +1,21 @@
-import { Divider, Fab, TextField } from "@mui/material";
+import { Divider, Fab, TextField, Tooltip } from "@mui/material";
 import { Box } from "@mui/system";
 import EditIcon from '@mui/icons-material/Edit';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 import PreviewIcon from '@mui/icons-material/Preview';
+import SaveIcon from '@mui/icons-material/Save';
+
+import CastleIcon from '@mui/icons-material/Castle';
+import PersonIcon from '@mui/icons-material/Person';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import GroupsIcon from '@mui/icons-material/Groups';
+import ChurchIcon from '@mui/icons-material/Church';
+import JoinFullIcon from '@mui/icons-material/JoinFull';
+
 import showdown from 'showdown'
 
 import { useState } from 'react';
+import { Link } from "react-scroll";
 
 const inputBox = (props) => {
     return (
@@ -18,7 +28,7 @@ const inputBox = (props) => {
                     defaultValue={props.textContent}
                     fullWidth
                     multiline
-                    minRows={20}
+                    minRows={10}
                     onChange={(event) => {
                         props.setTextContent(event.target.value);
                     }}
@@ -27,11 +37,48 @@ const inputBox = (props) => {
     )
 }
 
+const contentsList = (props) => {
+    let contents = props.textContent.split('\n').map(line => {
+        if (line[0] === "#") {
+            const title = line.split(' ');
+            const level = title[0].length;
+            const content = title.slice(1,).join(' ');
+            return {level: level, content: content}
+        }
+    });
+    contents = contents.filter(e => e != null);
+
+    return (
+        <Box>
+            <h2>Contents</h2>
+            {contents.map((heading, index) => {
+            const id = heading.content.split(' ').join('-')
+            console.log(id)
+            return (
+                <p key={index}>
+                    <Link
+                        to={id}
+                        smooth={true}
+                    >
+                        {heading.content}
+                    </Link>
+                </p>
+            )})}
+            <Divider />
+        </Box>
+        
+    )
+}
+
 const outputBox = (props) => {
+    const textContent = props.textContent;
     var converter = new showdown.Converter();
-    const html = converter.makeHtml(props.textContent)
+    converter.setFlavor('github');
+    const html = converter.makeHtml(textContent);
+
     return (
         <Box sx={{width: "90%"}}>
+            {contentsList({textContent})}
             <p>
                 <div dangerouslySetInnerHTML={{ __html: html}}></div>
             </p>
@@ -43,6 +90,15 @@ const ContentEditor = () => {
     const [editState, setEditState] = useState(true);
     const [previewState, setPreviewState] = useState(false);
     const [textContent, setTextContent] = useState("");
+
+    const templates = [
+        {name: "Settlement", icon: <CastleIcon/>},
+        {name: "NPC", icon: <PersonIcon/>},
+        {name: "Geographical Feature", icon: <LandscapeIcon/>},
+        {name: "Faction", icon: <GroupsIcon/>},
+        {name: "Religion", icon: <ChurchIcon/>},
+        {name: "Plane of Existence", icon: <JoinFullIcon/>},
+    ]
 
     return (
         <Box>
@@ -65,35 +121,43 @@ const ContentEditor = () => {
                         gap: "20px",
                         minWidth: "70px",
                     }}>
-                    <Fab
-                        size="small"
-                        onClick={() => {
-                            if (editState) {
-                                setPreviewState(false);
-                            }
-                            setEditState(!editState);
-                        }}
-                    >
-                        <EditIcon/>
-                    </Fab>
-                    <Fab
-                        size="small"
-                        onClick={() => {
-                            setPreviewState(!previewState);
-                        }}
-                    >
-                        <PreviewIcon />
-                    </Fab>
-                    <Fab size="small">
-                        <HelpCenterIcon/>
-                    </Fab>
+                    <Tooltip title={editState ? "Save" : "Edit"}>
+                        <Fab
+                            size="small"
+                            onClick={() => {
+                                if (editState) {
+                                    setPreviewState(false);
+                                }
+                                setEditState(!editState);
+                            }}
+                        >
+                            {editState ? <SaveIcon/> : <EditIcon/>}
+                        </Fab>
+                    </Tooltip>
+                    <Tooltip title="Live Preview">
+                        <Fab
+                            size="small"
+                            onClick={() => {
+                                setPreviewState(!previewState);
+                            }}
+                        >
+                            <PreviewIcon />
+                        </Fab>
+                    </Tooltip>
+                    <Tooltip title="Help">
+                        <Fab 
+                            size="small"
+                        >
+                            <HelpCenterIcon/>
+                        </Fab>
+                    </Tooltip>
                     <Divider/>
                     {/* TODO: make templates that trigger using the buttons below */}
-                    <Fab size="small" />
-                    <Fab size="small" />
-                    <Fab size="small" />
-                    <Fab size="small" />
-                    <Fab size="small" />
+                    {templates.map(template => (
+                        <Tooltip key={template.name} title={template.name}>
+                            <Fab size="small">{template.icon}</Fab>
+                        </Tooltip>
+                    ))}
                 </Box>
                 {previewState && outputBox({textContent})}
             </Box>
